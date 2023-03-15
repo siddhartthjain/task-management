5cdd7a3987f8374adb2bfec1a9e7805f0368d239
siddharth <siddhartthjain@gmail.com>
2023-03-15 23:31:12 +0530

    deployment

--- knexfile.ts
+++ knexfile.ts
@@ -6,39 +6,39 @@ import {Knex} from "knex";
 
 const config: { [key: string]: Knex.Config } = {
 
-  development: {
-    client: 'mysql',
-    connection: {
-      host: 'localhost',
-      port: 3306,
-      user: 'root',
-      password: 'admin',
-      database: 'task_database',
-    },
-    migrations: {
-      directory: __dirname + '/src/database/migrations',
-      extension: 'ts',
-    },
-    seeds: {
-      directory: __dirname + '/src/database/seeds',
-    },
-  },
+  // development: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: 'localhost',
+  //     port: 3306,
+  //     user: 'root',
+  //     password: 'admin',
+  //     database: 'task_database',
+  //   },
+  //   migrations: {
+  //     directory: __dirname + '/src/database/migrations',
+  //     extension: 'ts',
+  //   },
+  //   seeds: {
+  //     directory: __dirname + '/src/database/seeds',
+  //   },
+  // },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
 
   production: {
     client: "mysql",
